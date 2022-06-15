7a6230727a96688e5f529c96f5c5efd56307bb2a
wes-mitchell <wmdrums@gmail.com>
2022-06-15 10:50:30 -0500

    begins app background color build

--- TabloidCLI/Program.cs
+++ TabloidCLI/Program.cs
@@ -1,6 +1,7 @@
 
 ﻿using TabloidCLI.UserInterfaceManagers;
 using System;
+using System.Collections.Generic;
 
 
 namespace TabloidCLI
@@ -9,7 +10,20 @@ namespace TabloidCLI
     {
         static void Main(string[] args)
         {
+            List<string> backgroundColors = new List<string>
+            {
+                "Black", "Dark Green", "Dark Yelow", "Dark Gray", "Cyan", "Magenta"
+            };
+            int index = 1;
             Console.WriteLine("Hello! Welcome to Tabloid!");
+            Console.WriteLine("--------------------------");
+            foreach (string color in backgroundColors)
+            {
+                Console.WriteLine($"{index}) {color}");
+                index++;
+            }
+            Console.WriteLine("Please provide the number of color to set your background color.");
+            Console.ReadLine()
             // MainMenuManager implements the IUserInterfaceManager interface
             IUserInterfaceManager ui = new MainMenuManager();
             while (ui != null)
